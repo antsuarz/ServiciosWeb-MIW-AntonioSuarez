5cc41736573ed035c25850d47a95e9d1611fcf9f
ANTONIO SUAREZ CRESPO <UO270543@uniovi.es>
2024-02-20 18:40:24 +0100

    Cliente de Consola
    Cliente de Consola

---
+++ WS.ChargerService.Asturias/WS.ChargerService.Asturias.Consola/Program.cs
@@ -0,0 +1,57 @@
+﻿using AppAsturiasServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WS.ChargerService.Asturias.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationServicesClient client = new ApplicationServicesClient();
+            var cargadores = client.GetCargadoresAsync().Result;
+            DefMessages();
+            while (true)
+            {
+                string response = Console.ReadLine();
+                switch (response)
+                {
+                    case "all":
+                        foreach (CargadorModel c in cargadores) {
+                            Console.WriteLine(c.Title);
+                        }
+                        DefMessages();
+                        break;
+                    case "ovd":
+                        for(int i = 0; i < 50; i++)
+                        {
+                            Console.WriteLine(cargadores[i].Title);
+                        }
+                        DefMessages();
+                        break;
+                    case "gjn":
+                        for(int i = 50; i < cargadores.Length; i++)
+                        {
+                            Console.WriteLine(cargadores[i].Title);
+                        }
+                        DefMessages();
+                        break;
+                    default: 
+                        Console.WriteLine("Comando no reconocido (all, ovd, gjn)");
+                        break;
+                }
+            }           
+        }
+
+        static void DefMessages() {
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Introduzca all para ver los cargadores registrados.");
+            Console.WriteLine("Introduzca ovd para ver los cargadores de Oviedo registrados.");
+            Console.WriteLine("Introduzca gjn para ver los cargadores de Gijon  registrados.");
+        }
+
+    }
+}
